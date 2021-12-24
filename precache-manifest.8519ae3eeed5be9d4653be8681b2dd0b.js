5f46d580a69c4f3dc266e58ee17d5bf6417ec781
aayushi2001 <aayushi1485.cse19@chitkara.edu.in>
2021-12-24 19:31:32 +0530

    Updates

---
+++ precache-manifest.8519ae3eeed5be9d4653be8681b2dd0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f3c8514acb803ff94c4b855f67c2ef",
+    "url": "/E-commerce-Site/index.html"
+  },
+  {
+    "revision": "7245935c4d2cb85982b5",
+    "url": "/E-commerce-Site/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5953e1cec135d43dd2bd",
+    "url": "/E-commerce-Site/static/css/main.32bbb75a.chunk.css"
+  },
+  {
+    "revision": "7245935c4d2cb85982b5",
+    "url": "/E-commerce-Site/static/js/2.cc56c318.chunk.js"
+  },
+  {
+    "revision": "5953e1cec135d43dd2bd",
+    "url": "/E-commerce-Site/static/js/main.376ebdbd.chunk.js"
+  },
+  {
+    "revision": "f34311d53da53a579ef0",
+    "url": "/E-commerce-Site/static/js/runtime~main.b89b2464.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/E-commerce-Site/static/media/logo.9f7e506d.svg"
+  }
+]);
